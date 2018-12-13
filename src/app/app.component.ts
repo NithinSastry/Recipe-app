c1fd940d836c4ef551db11132b2cb2f9785a3d03
telln <nithinsastry.tellapuri@in.pega.com>
2018-12-13 18:13:09 +0530

    Initial project setup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'learn-app';
+  title = 'My first app';
 }
---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("test", "test desc", ""),
+    new Recipe("test1", "test desc", ""),
+    new Recipe("test2", "test desc", ""),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
