217fd5f4d8c3aca8769a03b1d7639ed238d69f8d
telln <nithinsastry.tellapuri@in.pega.com>
2018-12-18 18:10:42 +0530

    Consuming recipe service in recipe-list.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,20 +8,22 @@ import { Recipe } from "../recipe.model";
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe("test", "test desc", ""),
-    new Recipe("test1", "test desc", ""),
-    new Recipe("test2", "test desc", ""),
-  ];
+  // recipes: Recipe[] = [
+  //   new Recipe("test", "test desc", ""),
+  //   new Recipe("test1", "test desc", ""),
+  //   new Recipe("test2", "test desc", ""),
+  // ];
+  recipes: Recipe[];
 
   @Output("recipeListItemClicked")
   recipeListItemClicked: EventEmitter<Recipe> = new EventEmitter<Recipe>();
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
-  onRecipeItemClicked(recipe: Recipe){
+  onRecipeItemClicked(recipe: Recipe) {
     this.recipeListItemClicked.emit(recipe);
   }
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,13 @@
-export class RecipeService{
-     
+import { Recipe } from './recipe.model';
+
+export class RecipeService {
+     private recipes: Recipe[] = [
+          new Recipe("test", "test desc", ""),
+          new Recipe("test1", "test desc", ""),
+          new Recipe("test2", "test desc", ""),
+     ];
+
+     getRecipes(){
+          return this.recipes.slice();
+     }
 }
