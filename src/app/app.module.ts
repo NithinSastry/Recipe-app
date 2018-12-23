8b3ab4dedd5c33d5fafa85365799761fc8eeca1f
NithinSastry <nithin_sastry@outlook.com>
2018-12-23 13:23:28 +0530

    Added basic routes to the app.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,7 +29,8 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule  
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [ShoppingListService],
   bootstrap: [AppComponent]
